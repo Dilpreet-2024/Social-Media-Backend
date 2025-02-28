d85b67f9fe8f351fbf8c6fb6819d50d94051cd19
dilpreet2012 <dilkill2000@gmail.com>
2025-02-28 17:23:38 +0530

    aggragation pipeline queries for subscriber model

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -196,7 +196,7 @@ catch(err)
  export const getCurrentuser=asyncHandler(async(req,res)=>{
     return res.status(200)
     .json(
-        200,req.user,"current user fetched successfully"
+        new ApiResponse(200,req.user,"current user fetched successfully")
     )
  })
  export const updateAccountDetails=asyncHandler(async(req,res)=>{
@@ -264,5 +264,76 @@ export const updateCoverImage=asyncHandler(async(req,res)=>{
         {
             new:true
         }
-    )   
+    ) 
+    return res.status(200).
+     json(new ApiResponse(200,user,"Cover Image updated successfully"))  
+})
+export const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {userName}=req.params;
+    if(!userName?.trim())
+    {
+        throw new ApiError(400,"Username is missing");
+    }
+    const channel=await User.aggregate([
+        {
+$match:{
+    userName:userName?.toLowerCase()
+}},
+{
+$lookup:{
+    from:"subscriptions",
+    localField:"_id",
+    foreignField:"channel",
+    as:"subscribers"
+}
+},
+{
+    $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribedTo"
+    }
+},
+{
+    $addFields:
+    {
+        subscribersCount:{
+            $size:"$subscribers",
+        },
+        channelsSubscribedToCount:{
+            $size:"$subscribedTo"
+        },
+        isSubscribed:{
+            $cond:
+            {
+                if:{
+                    $in:[req.user?._id,"$subscribers.subscriber"]
+                },
+                then:true,
+                else:false
+            }
+        }
+    }
+},
+{
+    $project:{
+        fullName:1,
+        userName:1,
+        subscribersCount:1,
+        channelsSubscribedToCount:1,
+        isSubscribed:1,
+        avatar:1,
+        coverImage:1,
+        email:1
+    }
+}
+    ])
+    if(!channel?.length)
+    {
+        throw new ApiError("channel does not exist");
+    }
+    return res.status(200).
+    json(new ApiResponse(200,channel[0],"User channel fetched successfully"));
+    
 })
